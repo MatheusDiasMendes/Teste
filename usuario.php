6a75ea23d64f28bd4b00251831601b21eebf3660
Matheus DIas <80039566+MatheusDiasMendes@users.noreply.github.com>
2022-02-21 10:28:47 -0300

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -5,7 +5,7 @@
     <link href="estilo.css" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Teste</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     
 </head>
